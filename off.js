bdc5c23050f4d5d1a50ec228e49857c32ec57cc4
RROSE <ronald.de.castelbajac@gmail.com>
2024-03-04 16:23:23 +0100

    try fixing onboundary mobile

---
+++ off.js
@@ -0,0 +1,25 @@
+<div className="modal-overlay">
+    <div className="modal">
+        <button className="modal-close-button" onClick={onClose}>close</button>
+        <h4>subscribe to our newsletter</h4>
+        <form action="https://ximonlee.us22.list-manage.com/subscribe/post?u=52e3cb90c16ac3bc074a9245a&amp;id=f750569e53&amp;f_id=00dbc3e1f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank">
+
+
+            <div className="mc-field-group">
+                <input type="email" placeholder="enter your email" required />
+            </div>
+            {/* <input type="submit" name="subscribe" id="mc-embedded-subscribe" className="button" value="Subscribe" /> */}
+            <div className="optionalParent">
+                <div className="clear foot">
+                    <input type="submit" name="subscribe" id="mc-embedded-subscribe" className="button" value="Subscribe" />
+                </div>
+            </div>
+
+            <div id="mce-responses" className="clear foot">
+                <div className="response" id="mce-error-response" style={{ display: 'none' }}></div>
+                <div className="response" id="mce-success-response" style={{ display: 'none' }}></div>
+            </div>
+            {/*                     <button type="submit" className=''>send</button>
+*/}                </form>
+    </div>
+</div>
